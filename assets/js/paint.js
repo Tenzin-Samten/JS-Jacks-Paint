39805c351ce2a4fb3bc700874efa499817f2a8c0
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-03-14 17:36:23 -0400

    final

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,23 +1,22 @@
 function configureListeners() {
-    var images = document.getElementsByTagName('img');
-
+    var images = document.getElementsByTagName('img');    
 
      for (var i = 0; i < images.length; i++) {        
-       document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)
-       document.getElementById(images[i].id).addEventListener('mouseover', removeOpacity, false)   
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
     } 
 }
 
 function addOpacity(event) {
     if (!this.classList.contains('dim')){
         this.classList.add('dim')
-    }
+    }    
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
-     if (!this.classList.contains('dim')){
-        this.classList.add('dim')
+     if (this.classList.contains('dim')){
+        this.classList.remove('dim');
     }
 
     let element = document.getElementById('color-price');
@@ -35,49 +34,49 @@ function getProductInfo(partNumber) {
     
     switch (partNumber) {
         case 'pn1':           
-            price = '14.99' 
-            colorName = 'Lime Green' 
-            updatePrice(colorName, price)  
+            price = '$19.99'
+            colorName = 'Lime Green'
+            updatePrice(colorName, price)           
             break;           
         case 'pn2':
-            price = '11.14' 
+            price = '$12.99'
             colorName = 'Medium Brown' 
             updatePrice(colorName, price)     
             break;            
         case 'pn3':
-            price = '22.99' 
-            colorName = 'ROyal Blue' 
-            updatePrice(colorName, price) 
+            price = '$11.99'
+            colorName = 'Royal Blue'   
+            updatePrice(colorName, price)   
             break;   
         case 'pn4':
-            price = '4.99'
-            colorName = 'Solid Black' 
-            updatePrice(colorName, price)  
+            price = '$14.99'
+            colorName = 'Bright Red'            
+            updatePrice(colorName, price)   
             break;   
         case 'pn5':
-            price = '8.22' 
-            colorName = 'Solid Cyan' 
-            updatePrice(colorName, price)       
+            price = '$9.99'
+            colorName = 'Solid White' 
+            updatePrice(colorName, price)              
             break;   
         case 'pn6':
-            price = '11.99' 
-            colorName = 'Solid Purple' 
-            updatePrice(colorName, price)         
+            price = '$15.99'
+            colorName = 'Solid Black'   
+            updatePrice(colorName, price)            
             break;   
         case 'pn7':
-            price = '13.42' 
-            colorName = 'Solid Red' 
-            updatePrice(colorName, price)
+            price = '$8.99'
+            colorName = 'Medium Blue'       
+            updatePrice(colorName, price)        
             break;   
         case 'pn8':
-            price = '21.98' 
-            colorName = 'Solid White' 
-            updatePrice(colorName, price)  
+            price = '$16.99'
+            colorName = 'Light Purple'      
+            updatePrice(colorName, price)         
             break;   
         case 'pn9':
-            price = '14.99' 
-            colorName = 'Solid Yeloow' 
-            updatePrice(colorName, price)
+            price = '$17.99'
+            colorName = 'Bright Yellow'                       
+            updatePrice(colorName, price)   
             break;   
           default:              
     }
